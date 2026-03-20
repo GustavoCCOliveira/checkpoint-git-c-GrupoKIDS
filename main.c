327b00899a41ef216ae88832914e897a3626177d
Diogocsantosz <chiaradiadiogo205@gmail.com>
2026-03-20 12:08:57 -0300

    adcionado os nomes dos particpantes do gp

--- main.c
+++ main.c
@@ -5,8 +5,10 @@ int main(){
 
     printf("ola mundo");
     int dinheiro = 1000;
-     printf("qual é seu salário", dinheiro);
-    int idade = 20;
+    printf("Diogo rm:570246, rafael rm:561975, lucas rm:568690, Gustavo rm:572952, Pedro rm:570083");
+    printf("qual é seu salário", dinheiro);
+    
+     int idade = 20;
     printf("qual a idade", idade);
     int c = 2 + 2;
 
